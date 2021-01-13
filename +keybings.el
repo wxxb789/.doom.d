92161b856c3f4822deebb073e6ad71eb38e1ce1b
wxxb789 <wxxb789@outlook.com>
2021-01-13 23:02:45 +0800

    add keybings

--- +keybings.el
+++ +keybings.el
@@ -1,9 +1,11 @@
 ;;; +keybings.el -*- lexical-binding: t; -*-
 
-;; comma leader define
+;; {{ comma leader define
 (map!
+ :nv ",," 'evilnc-comment-operator
  :nv ",ci" 'evilnc-comment-or-uncomment-lines
- :nv ",cl" 'evilnc-quick-comment-or-uncomment-to-the-line
  :nv ",cc" 'evilnc-copy-and-comment-lines
- :nv ",cp" 'my-evilnc-comment-or-uncomment-paragraphs
+ ;; :nv ",cl" 'evilnc-quick-comment-or-uncomment-to-the-line
+ ;; :nv ",cp" 'my-evilnc-comment-or-uncomment-paragraphs
  )
+;; }}
