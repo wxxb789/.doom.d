938e63df7b7f8752f4211a79f46ce6cf4328bdb7
wxxb <wxxb789@outlook.com>
2021-01-14 16:00:08 +0800

    use "Inconsolata NF" as English Font.

--- +ui.el
+++ +ui.el
@@ -28,9 +28,9 @@
 ;; (message doom-unicode-extra-fonts)
 ;; (setq doom-unicode-extra-fonts (seq-difference doom-unicode-extra-fonts '("FontAwesome" "file-icons" "github-octicons")))
 
-(setq doom-font (font-spec :family "Sarasa Mono SC" :size 32)
+(setq doom-font (font-spec :family "Inconsolata NF" :size 32)
       doom-serif-font (font-spec :family "Sarasa Mono SC")
-      doom-variable-pitch-font (font-spec :family "Sarasa Mono SC")
+      doom-variable-pitch-font (font-spec :family "Inconsolata NF")
       doom-unicode-font (font-spec :family "Sarasa Mono SC")
       doom-big-font (font-spec :family "Sarasa Mono SC" :size 38))
 
